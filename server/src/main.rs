2cc20c8338b364fd85d8310bc01470a4e152c3b8
Anthony Di Taranto <ajdt703@gmail.com>
2022-02-23 15:38:05 -0800

    Add CORS handling

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,6 +8,28 @@ mod ports;
 
 use crtshentry::CrtShEntry;
 use domain::Domain;
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Attaching CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[get("/<domain_name>")]
 async fn enumerate_subdomains(domain_name: &str) -> String {
@@ -44,6 +66,7 @@ fn find_open_ports(domain: &str) -> String {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(CORS)
         .mount(
             "/", 
             routes![
